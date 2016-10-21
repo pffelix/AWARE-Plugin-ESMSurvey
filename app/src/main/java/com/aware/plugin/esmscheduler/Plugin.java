46e01f7c4a8ee89100e2f937408c36b6ec1cbc8b
ru67zid <felix.pfreundtner@campus.lmu.de>
2016-10-21 17:58:53 +0200

    "updated instructions for mESM"

--- app/src/main/java/com/aware/plugin/esmscheduler/Plugin.java
+++ app/src/main/java/com/aware/plugin/esmscheduler/Plugin.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+
 import org.json.JSONException;
+
+import java.util.Locale;
+import java.lang.String;
 
 
 import com.aware.Aware;
@@ -73,45 +77,64 @@ public class Plugin extends Aware_Plugin {
     }
 
     private void scheduleMorningQuestionnaire() {
-        try {
+        String esm_instruction_1 = "a";
+        String esm_instruction_2 = "b";
+        String lang_1 = Locale.getDefault().getLanguage();
+        String lang_2 = Locale.getDefault().getDisplayLanguage();
+        if ("ger".equalsIgnoreCase(lang_1) || "deu".equalsIgnoreCase(lang_1) || "de".equalsIgnoreCase(lang_1) || "German".equalsIgnoreCase(lang_1) || "Deutsch".equalsIgnoreCase(lang_1) || "ger".equalsIgnoreCase(lang_2)|| "deu".equalsIgnoreCase(lang_2)|| "de".equalsIgnoreCase(lang_2) || "German".equalsIgnoreCase(lang_2)|| "Deutsch".equalsIgnoreCase(lang_2)) {
+            esm_instruction_1 = "'esm_instructions':'Inwieweit stimmst du folgender Aussage zu: Gerade ist mir langweilig?'";
+            esm_instruction_2 = "'esm_instructions':'Inwieweit stimmst du folgender Aussage zu: (Gerade bin ich gestresst??)'";
+        } else {
+            esm_instruction_1 = "'esm_instructions':'To what extend do you agree to the statement?: \n Right now, I feel bored?',\n";
+            esm_instruction_2 = "'esm_instructions':'To what extend do you agree to the statement?: \n Right now, I feel stressed?',\n";
+        }
 
-            String esm_goodmorning = "[{'esm': {\n" +
+        try {
+            esm_instruction_1= "'esm_instructions': 'test1',\n";
+            esm_instruction_2= "'esm_instructions': 'test2',\n";
+
+            String esm_questions = "[{'esm': {\n" +
                     "'esm_type':6,\n" +
                     "'esm_title':'ESM Scale',\n" +
-                    "'esm_instructions':'To what extend do you agree to the statement: \n Right now, I feel bored?',\n" +
+                    esm_instruction_1 +
                     "'esm_scale_min':0,\n" +
-                    "'esm_scale_max':100,\n" +
+                    "'esm_scale_max':10,\n" +
                     "'esm_scale_start':0,\n" +
                     "'esm_scale_max_label':'agree',\n" +
                     "'esm_scale_min_label':'disagree',\n" +
                     "'esm_scale_step':1,\n" +
                     "'esm_submit':'Next',\n" +
-                    "'esm_expiration_threshold':60,\n" +
+                    "'esm_expiration_threshold':179,\n" +
+                    "'esm_notification_timeout':179,\n" +
                     "'esm_trigger':'AWARE Tester'\n" +
                     "}}, {'esm': { " +
                     "'esm_type':6,\n" +
                     "'esm_title':'ESM Scale',\n" +
-                    "'esm_instructions':'To what extend do you agree to the statement: \n Right now, I feel bored(2)?',\n" +
+                    esm_instruction_2 +
                     "'esm_scale_min':0,\n" +
-                    "'esm_scale_max':100,\n" +
+                    "'esm_scale_max':10,\n" +
                     "'esm_scale_start':0,\n" +
                     "'esm_scale_max_label':'agree',\n" +
                     "'esm_scale_min_label':'disagree',\n" +
                     "'esm_scale_step':1,\n" +
                     "'esm_submit':'OK',\n" +
-                    "'esm_expiration_threshold':60,\n" +
+                    "'esm_expiration_threshold':179,\n" +
+                    "'esm_notification_timeout':179,\n" +
                     "'esm_trigger':'AWARE Tester'\n" +
                     "}}]";
 
             Scheduler.Schedule schedule = new Scheduler.Schedule("morning_question");
             schedule.addHour(9); //we want this schedule every day at 9, 12, 15, 18, 21
-            schedule.addHour(12); //we want this schedule every day at 9, 12, 15, 18, 21
+            schedule.addHour(12);
+            schedule.addHour(15);
+            schedule.addHour(18);
+            schedule.addHour(21);
 
 //            schedule.addMinute(40);
 //            schedule.setInterval(4);
             schedule.setActionType(Scheduler.ACTION_TYPE_BROADCAST); //we are doing a broadcast
             schedule.setActionClass(ESM.ACTION_AWARE_QUEUE_ESM); //with this action
-            schedule.addActionExtra(ESM.EXTRA_ESM, esm_goodmorning); //and this extra
+            schedule.addActionExtra(ESM.EXTRA_ESM, esm_questions); //and this extra
             Scheduler.saveSchedule(getApplicationContext(), schedule);
 
         } catch (JSONException e) {
